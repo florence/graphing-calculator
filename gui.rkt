ebe5666d1ac3cf19adf1a91dd82ca74b2d572d2e
Spencer Florence <florence@ccs.neu.edu>
2013-06-20 00:24:23 -0400

    cleaned up this POS gui a bit

--- gui.rkt
+++ gui.rkt
@@ -4,18 +4,13 @@
 (provide main)
 (define (main) (show))
 
+;; -> Void
+;; Show a new graphing calculator 
 (define (show)
   ;; main frame
-  (define frame
-    (new frame% [label "A Basic Calculator"]))
+  (define frame (new frame% [label "A Basic Calculator"]))
   ;; input fields
-  (define-values (get graph) (make-grapher frame))
-  (define min (new text-field% [label "min"] [parent frame] [init-value "-10"]))
-  (define max (new text-field% [label "max"] [parent frame] [init-value "10"]))
-  (define inverse?
-    (new check-box%	 
-         [label "invert?"]	 
-         [parent frame]))
+  (define graph (make-grapher frame))
   ;; rendering
   (define render (make-renderer frame))
   ;; the button
@@ -25,52 +20,70 @@
          [callback 
           (lambda (button event)
             (with-handlers ([exn:fail? show-error-dialog])
-              (render
-               (apply graph
-                      (string->number (send min get-value))
-                      (string->number (send max get-value))
-                      (send inverse? get-value)
-                      (get)))))]))
+              (render (graph))))]))
   ;; go
   (send frame show #t))
 
-;; parentable -> grapher (-> string)
+(define choices-names '("normal" "parametric" "polar"))
+(define choices-functions (list graph2d parametric2d polar2d))
+(define choices-labels '(("f(x) = ") ("x(t) = " "y(t) = ") ("r(t) = ")))
+
+;; parentable -> (-> image-snip%)
+;; take a parent object attached the fields needed for getting user input
+;; and return a function for generating an image-snip% from user input
 (define (make-grapher parent)
-  (define (fields* . str) 
-    (call-with-values (λ () (apply function-fields container str)) list))
-  
   (define container (new pane% [parent parent]))
-  (define choices
-    (hash "normal" (cons graph2d (fields* "f(x) = "))
-          "parametric" (cons parametric2d (fields* "x(t) = " "y(t) = "))
-          "polar" (cons polar2d (fields* "r(t) = "))))
+  (define min (new text-field% [label "min"] [parent parent] [init-value "-10"]))
+  (define max (new text-field% [label "max"] [parent parent] [init-value "10"]))
+  (define inverse? (new check-box% [label "invert?"] [parent parent]))
   
-  (define getb (box void))
-  (define graphb (box void))
-  (define deactiveb (box void))
+  (define (fields* . strs) 
+    (call-with-values (λ () (apply function-fields container strs)) list))
+  
+  (define choices-map
+    (for/hash ([n choices-names] [f choices-functions] [l choices-labels])
+      (values n (cons f (apply fields* l)))))
+  
+  (define get void)
+  (define graph void)
+  (define deactive void)
   (define (update-to! s)
-    (define-values (grapher e getter) (apply values (hash-ref choices s)))
-    ((unbox deactiveb) #f)
-    (e #t)
-    (set-box! deactiveb e)
-    (set-box! getb getter)
-    (set-box! graphb grapher))
+    (define-values (grapher activate getter) (apply values (hash-ref choices-map s)))
+    (deactive)
+    (activate)
+    (set! deactive activate)
+    (set! get getter)
+    (set! graph grapher))
   
   (define chooser
-    (new choice% [parent parent] [label "function type"] [choices (hash-keys choices)]
+    (new choice% [parent parent] [label "function type"] [choices choices-names]
          [callback (λ (c _) (update-to! (send c get-string-selection)))]))
+  
   (update-to! (send chooser get-string-selection))
-  (values (λ _ (apply (unbox getb) _)) (λ _ (apply (unbox graphb) _))))
+  
+  (λ ()
+    (apply graph
+           (string->number (send min get-value))
+           (string->number (send max get-value))
+           (send inverse? get-value)
+           (get))))
 
-;; parentable string... -> (bool ->) (-> (listof strings))
+;; parentable string... -> (->) (-> (listof strings))
+;; add one text field to the parent for each name (using the given name as the label)
+;; the fields are hidden
+;; return a function to show/hide the fields and a function to get values from all of them
 (define (function-fields parent . names)
   (define p (new horizontal-panel% [parent parent] [style '(deleted)]))
-  (define functions (map (λ (name) (new text-field% [label name] [parent p])) names))
+  (define functions (for/list ([name names]) (new text-field% [label name] [parent p])))
   (values
-   (λ (?) (if ? (send parent add-child p) (send parent delete-child p)))
+   (let ([? #t])
+     (λ () (if ? (send parent add-child p) (send parent delete-child p)) 
+       (set! ? (not ?))))
    (λ () (map (λ (f) (send f get-value)) functions))))
 
 ;; parentable -> (image-snip% ->)
+;; add a canvas to the parent
+;; return a function that will render an image-snip% to the canvas
 (define (make-renderer parent)
   (define paste  (new pasteboard%))
   (define canvas (new editor-canvas%
@@ -91,25 +104,15 @@
 ;; -> (Nat Board -> Void)
 ;; display current state in a canvas
 (define (show-error-dialog e)
-  ; Create a dialog
   (define dialog (instantiate dialog% ("Error")))
-  
-  ; Add a text field to the dialog
   (new message% [parent dialog] [label (exn->label-string e)])
+  (define panel (new horizontal-panel% [parent dialog] [alignment '(center center)]))
+  (new button% [parent panel] [label "Ok"] [callback (λ _ (send dialog show #f))])
   
-  ; Add a horizontal panel to the dialog, with centering for buttons
-  (define panel (new horizontal-panel% [parent dialog]
-                     [alignment '(center center)]))
-  
-  ; Add Cancel and Ok buttons to the horizontal panel
-  (new button% [parent panel] [label "Ok"]
-       [callback (λ _ (send dialog show #f))])
-  (when (system-position-ok-before-cancel?)
-    (send panel change-children reverse))
-  
-  ; Show the dialog
   (send dialog show #t))
 
+;; exn? -> label-string?
+;; returns first 200 chars of the exn's message
 (define (exn->label-string e)
   (define s (~a (exn-message e)))
   (if (<= (string-length s) 200)
