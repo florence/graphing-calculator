24235ef21c4a3649faa7d3531c730f64779dec57
Spencer Florence <florence@ccs.neu.edu>
2013-06-20 16:57:02 -0400

    disabled dragging of the graph, so that you can zoom

--- gui.rkt
+++ gui.rkt
@@ -81,7 +81,9 @@
 ;; add a canvas to the parent
 ;; return a function that will render an image-snip% to the canvas
 (define (make-renderer parent)
-  (define paste  (new pasteboard%))
+  (define paste  (new (class pasteboard%
+                        (super-new)
+                        (define/augment (can-interactive-move? e) #f))))
   (define canvas (new editor-canvas%
                       [parent parent]
                       [editor paste]
